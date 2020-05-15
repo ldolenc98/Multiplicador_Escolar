2e6e8d1dc24b71916ad44a41ee3bbfaa528ba406
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-05-15 12:53:28 -0300

    Add files via upload

---
+++ teste_multiplicador.py
@@ -0,0 +1,25 @@
+from unittest import TestCase, main
+from mult_escola_luciano import multiplica
+
+class Testes(TestCase):
+
+	"""def teste_espaco_multiplicando(self):
+		self.assertEqual(multiplica(-3242343,-594), "        ")
+		self.assertEqual(multiplica(493,-5345), "     ")
+		self.assertEqual(multiplica(38271,4875), "       ")"""
+
+	"""def teste_espaco_multiplicador(self):
+		self.assertEqual(multiplica(-3242343,-594), "  ")
+		self.assertEqual(multiplica(493,-5345), "     ")
+		self.assertEqual(multiplica(38271,4875), "    ")"""
+
+	"""def teste_traco(self):
+		self.assertEqual(multiplica(-3242343,-594), "------------")
+		self.assertEqual(multiplica(493,-5345), "----------")
+		self.assertEqual(multiplica(38271,4875), "-----------")"""
+
+
+
+if __name__ == '__main__':
+    main()
+
